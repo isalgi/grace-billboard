f6de4d425e143c964a0fb49cd5d65d75b0d3ab56
Faishal Makarim <isal@wongmakmur.com>
2025-08-28 09:14:14 +0700

    add

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,21 +1,13 @@
 function ServiceCard({ image, title, description, link }) {
   return (
-    <div className="bg-white rounded-[25px] shadow-sm p-3 text-center flex flex-col h-full">
+    <div className="bg-white rounded-[25px] shadow-sm p-3 text-center flex flex-col h-full min-h-[400px]">
       <img
         src={image}
-        className="w-full h-48 object-cover rounded-[25px] mb-3"
+        className="w-full h-48 object-cover rounded-[25px] mb-5"
         alt={title}
       />
       <h3 className="font-medium text-base mb-2 text-[#0C098C]">{title}</h3>
-      <p
-        className="text-[#000000] mb-3 px-1 text-left"
-        style={{
-          fontWeight: 400,
-          fontSize: "12px",
-          lineHeight: "21px",
-          letterSpacing: "0%",
-        }}
-      >
+      <p className="text-[#000000] mb-3 px-1 py-6 text-left leading-[18px] text-[12px]">
         {description}
       </p>
       <button className="bg-[#0C098C] text-white px-4 py-2 rounded-md text-xs hover:bg-[#0A0678] transition-colors mt-auto">
