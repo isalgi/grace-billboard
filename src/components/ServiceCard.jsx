548010bc98f98733af1d9a76c7aec4bff0b20f15
Faishal Makarim <isal@wongmakmur.com>
2025-08-28 09:07:44 +0700

    add

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,14 +1,12 @@
 function ServiceCard({ image, title, description, link }) {
   return (
-    <div className="bg-white rounded-xl shadow-sm p-3 text-center flex flex-col h-full">
+    <div className="bg-white rounded-[25px] shadow-sm p-3 text-center flex flex-col h-full">
       <img
         src={image}
-        className="w-full h-32 object-cover rounded-lg mb-3"
+        className="w-full h-48 object-cover rounded-[25px] mb-3"
         alt={title}
       />
-      <h3 className="font-medium text-base mb-2 text-[#0C098C] px-20">
-        {title}
-      </h3>
+      <h3 className="font-medium text-base mb-2 text-[#0C098C]">{title}</h3>
       <p
         className="text-[#000000] mb-3 px-1 text-left"
         style={{
@@ -20,7 +18,7 @@ function ServiceCard({ image, title, description, link }) {
       >
         {description}
       </p>
-      <button className="bg-[#0C098C] text-white px-4 py-1.5 rounded-md text-xs hover:bg-[#0A0678] transition-colors mt-auto">
+      <button className="bg-[#0C098C] text-white px-4 py-2 rounded-md text-xs hover:bg-[#0A0678] transition-colors mt-auto">
         Buka Area
       </button>
     </div>
--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -5,35 +5,44 @@ function ServicesSection() {
     {
       image: "/src/assets/image-09.webp",
       title: "Jl. Perintis Kemerdekaan",
-      description: "Berada di jalan protokol (Protokol B), dapat hilir Taman/Makarta cabang Kemayoran Jakarta Pusat sebesar kurang lebih 4KM, dekat dengan Mall Bola, Plaza Kuning, Komplex Jakarta Trade Center, menuju Kelapa Gading area ramai lainnya. View dari arah Cemara Pulih/Kelapa Gading menuju Priok."
+      description:
+        "Berada di jalan protokol (Protokol B), dapat hilir Taman/Makarta cabang Kemayoran Jakarta Pusat sebesar kurang lebih 4KM, dekat dengan Mall Bola, Plaza Kuning, Komplex Jakarta Trade Center, menuju Kelapa Gading area ramai lainnya. View dari arah Cemara Pulih/Kelapa Gading menuju Priok.",
     },
     {
       image: "/src/assets/image-03.webp",
       title: "Jl. Ahmad Yani Cempaka Putih",
-      description: "Billboard berada di jalan protokol (Protokol A), berada service di dekat dengan PD. Pasar Senen, Gedung Bappenas, dekat dengan Gedung Garmin, Universitas Jayabaya, PTC (Thamrinplaza Trade Center), pool B bis besar AKAR, area transito industri dan perdagangan, menuju ke banyak pusat stasium pusat Induk garut Senen. View dari arah Cemara Pulih/Kelapa Gading Tanjung Priok."
+      description:
+        "Billboard berada di jalan protokol (Protokol A), berada service di dekat dengan PD. Pasar Senen, Gedung Bappenas, dekat dengan Gedung Garmin, Universitas Jayabaya, PTC (Thamrinplaza Trade Center), pool B bis besar AKAR, area transito industri dan perdagangan, menuju ke banyak pusat stasium pusat Induk garut Senen. View dari arah Cemara Pulih/Kelapa Gading Tanjung Priok.",
     },
     {
       image: "/src/assets/image-07.webp",
       title: "Jl. Matraman Raya",
-      description: "Jl. Teritikad persisi di depan Soto Dunia Mas (Matraman), dekat dengan Mall Jakarta Kampung Jakarta, Kelurahan Utan Kayu Selatan, Kelurahan Utan Kayu Utara, Gramedia Matraman, Pasar Statium, dll. area pusat keramaian dan perkantoran. Memiliki kedataan pejalan jalan/kampung Melayu menuju Saldmbaya/Pumangi/Senen."
+      description:
+        "Jl. Teritikad persisi di depan Soto Dunia Mas (Matraman), dekat dengan Mall Jakarta Kampung Jakarta, Kelurahan Utan Kayu Selatan, Kelurahan Utan Kayu Utara, Gramedia Matraman, Pasar Statium, dll. area pusat keramaian dan perkantoran. Memiliki kedataan pejalan jalan/kampung Melayu menuju Saldmbaya/Pumangi/Senen.",
     },
     {
       image: "/src/assets/image-01.webp",
       title: "Jl. Raya Bekasi Pulo Gadung",
-      description: "Terletak di tempat strategis, dekat dengan Mall PTC (Pulogadung Trade Center), pool B bis besar AKAR, area kawasan industri dan perdagangan, menuju ke banyak pusat ekonomi lainnya. View dari arah Cakung/Derzpen Induk menuju Senen/Gambir/Pramoedya HI menuju Kota Tanah Abang."
+      description:
+        "Terletak di tempat strategis, dekat dengan Mall PTC (Pulogadung Trade Center), pool B bis besar AKAR, area kawasan industri dan perdagangan, menuju ke banyak pusat ekonomi lainnya. View dari arah Cakung/Derzpen Induk menuju Senen/Gambir/Pramoedya HI menuju Kota Tanah Abang.",
     },
     {
       image: "/src/assets/image-05.webp",
       title: "Jl. Kebon Kacang Raya",
-      description: "Billboard berada di dekat jantung kota Jakarta Pusat. Terletak di Mall Plaza Indonesia, Mall Grand Indonesia. Dekat perkantoran & swalayan menuju ke Senen/Tanah Abang HI menuju Kota/Muanyar/Tanah Abang."
-    }
+      description:
+        "Billboard berada di dekat jantung kota Jakarta Pusat. Terletak di Mall Plaza Indonesia, Mall Grand Indonesia. Dekat perkantoran & swalayan menuju ke Senen/Tanah Abang HI menuju Kota/Muanyar/Tanah Abang.",
+    },
   ];
 
   return (
     <section className="py-12 bg-gray-50">
-      <div className="max-w-5xl mx-auto px-4">
-        <h2 className="text-center text-xl font-medium mb-1 text-black">Available Area</h2>
-        <p className="text-center text-blue-700 font-medium mb-8 text-sm">JABODETABEK</p>
+      <div className="max-w-4xl mx-auto px-4">
+        <h2 className="text-center text-xl font-medium mb-1 text-black">
+          Available Area
+        </h2>
+        <p className="text-center text-blue-700 font-medium mb-8 text-sm">
+          JABODETABEK
+        </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
           {services.slice(0, 3).map((service, index) => (
@@ -47,7 +56,7 @@ function ServicesSection() {
           ))}
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-2xl mx-auto mb-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-xl mx-auto mb-8 ">
           {services.slice(3).map((service, index) => (
             <div key={index + 3} className="h-full">
               <ServiceCard
