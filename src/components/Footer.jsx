1e87bde3dd09aacd0adc5a34f295d22250d691d2
Faishal Makarim <isal@wongmakmur.com>
2025-08-28 23:30:09 +0700

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
     <footer className="px-20 pt-16 pb-10 bg-[#F6F6F6] max-sm:px-5 max-sm:py-10">
-      <div className="flex justify-between items-start mb-16 max-sm:flex-col gap-8 max-w-6xl mx-auto">
+      <div className="flex justify-between items-center mb-16 max-sm:flex-col gap-8 max-w-6xl mx-auto">
         {/* Logo and Description */}
-        <div className="flex flex-col gap-4 max-w-[200px]">
+        <div className="flex flex-col gap-4 max-w-[200px] justify-center">
           <div className="flex items-center">
             <img
               src="/Bejanaanugerah 1.png"
@@ -16,47 +16,47 @@ function Footer() {
         {/* Office Location and Contact us */}
         <div className="flex flex-col gap-8">
           {/* Office Location */}
-          <div className="flex flex-col gap-3">
-            <div className="flex items-center gap-2">
-              <svg
-                className="w-5 h-5 text-[#0C098C]"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                  clipRule="evenodd"
-                />
-              </svg>
+          <div className="flex gap-2">
+            <svg
+              className="w-5 h-5 text-[#0C098C] mt-1 flex-shrink-0"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <div className="flex flex-col gap-3">
               <h3 className="text-xl font-semibold text-[#0C098C]">
                 Office Location
               </h3>
+              <p className="text-black leading-none tracking-normal text-sm font-normal">
+                Lorem ipsum dolor sit amet, consectetur adipiscing
+                <br /> elit. Nullam tincidunt sit amet
+              </p>
             </div>
-            <p className="text-black leading-none tracking-normal text-sm font-normal">
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              <br /> elit. Nullam tincidunt sit amet
-            </p>
           </div>
 
           {/* Contact us */}
-          <div className="flex flex-col gap-3">
-            <div className="flex items-center gap-2">
-              <svg
-                className="w-5 h-5 text-[#0C098C]"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-              </svg>
+          <div className="flex gap-2">
+            <svg
+              className="w-5 h-5 text-[#0C098C] mt-1 flex-shrink-0"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+            </svg>
+            <div className="flex flex-col gap-3">
               <h3 className="text-xl font-semibold text-[#0C098C]">
                 Contact us
               </h3>
+              <p className="text-black leading-none tracking-normal text-sm font-normal">
+                Lorem ipsum dolor sit amet, consectetur adipiscing
+                <br /> elit. Nullam tincidunt sit amet
+              </p>
             </div>
-            <p className="text-black leading-none tracking-normal text-sm font-normal">
-              Lorem ipsum dolor sit amet, consectetur adipiscing
-              <br /> elit. Nullam tincidunt sit amet
-            </p>
           </div>
         </div>
 
